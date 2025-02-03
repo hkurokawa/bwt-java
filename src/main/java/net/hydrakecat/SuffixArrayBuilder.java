2efbb3198a6748ddb412beda8cb7caf187935f66
Hiroshi Kurokawa <hirosh.kurokawa@gmail.com>
2025-02-03 22:49:09 +0900

    Implement Induced Sorting algorithm to build Suffix Array

--- src/main/java/net/hydrakecat/SuffixArrayBuilder.java
+++ src/main/java/net/hydrakecat/SuffixArrayBuilder.java
@@ -1,6 +1,10 @@
 package net.hydrakecat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class SuffixArrayBuilder {
   /**
@@ -10,31 +14,145 @@ public class SuffixArrayBuilder {
    * @return the suffix array for the provided string
    */
   public static int[] build(CharSequence str) {
-    Pair[] indexSubstrPairs = new Pair[str.length() + 1];
+    int[] chars = new int[str.length()];
     for (int i = 0; i < str.length(); i++) {
-      indexSubstrPairs[i] = new Pair(i, str.subSequence(i, str.length()));
+      chars[i] = str.charAt(i);
     }
-    indexSubstrPairs[str.length()] = new Pair(str.length(), "");
-    Arrays.sort(indexSubstrPairs);
-
-    int[] ret = new int[indexSubstrPairs.length];
-    for (int i = 0; i < indexSubstrPairs.length; i++) {
-      ret[i] = indexSubstrPairs[i].i;
-    }
-    return ret;
+    return suffixArray(chars);
   }
 
-  private static class Pair implements Comparable<Pair> {
-    final int i;
-    final CharSequence str;
+  static int[] suffixArray(int[] input) {
+    if (input.length == 0) {
+      return new int[] {};
+    }
+    if (input.length == 1) {
+      return new int[] {0};
+    }
+    // Increase the size by 1 for the sentinel char ($)
+    int n = input.length + 1;
 
-    private Pair(int i, CharSequence str) {
-      this.i = i;
-      this.str = str;
+    // Compute the cumulative frequency of the characters excluding the sentinel char ($)
+    int[] freq = new int[Character.MAX_VALUE];
+    for (int i : input) freq[i]++;
+
+    // Type of the suffixes (true: S, false: L)
+    boolean[] types = new boolean[n];
+    types[n - 1] = true; // The last suffix ("$") is always type S
+    types[n - 2] = false; // The second to the last suffix is always type L
+    for (int i = n - 3; i >= 0; i--) {
+      if (input[i] == input[i + 1]) {
+        types[i] = types[i + 1];
+      } else {
+        types[i] = input[i] < input[i + 1];
+      }
     }
 
-    @Override public int compareTo(Pair o) {
-      return CharSequence.compare(str, o.str);
+    // Find the LMS (Left-Most-S)
+    List<Integer> lms = new ArrayList<>();
+    for (int i = 1; i < n; i++) {
+      if (types[i] && !types[i - 1]) lms.add(i);
     }
+
+    // Put the LMS in the array making sure they are bin-sorted (i.e., sorted by only the first character)
+    int[] sa = new int[n];
+    {
+      Arrays.fill(sa, -1);
+      int[] idx = new int[freq.length];
+      System.arraycopy(freq, 0, idx, 0, freq.length);
+      for (int i = 1; i < idx.length; i++) idx[i] += idx[i - 1];
+      for (int i : lms) {
+        if (i == n - 1) continue; // Ignore the sentinel char ($)
+        int ch = input[i];
+        sa[idx[ch]] = i;
+        idx[ch]--;
+      }
+      // The sentinel char ($) is the smallest and is always placed at the head
+      sa[0] = n - 1;
+    }
+
+    // Do an induced sorting with the bin-sorted LMS
+    inducedSort(n, sa, input, freq, types);
+
+    // Now LMS-substrings are completely sorted.
+    // Construct a string that represents a concatenated LMS-substrings.
+    Set<Integer> lmsSet = new HashSet<>(lms);
+    int[] lmsOrder = new int[n];
+    Arrays.fill(lmsOrder, -1);
+    for (int i = 0; i < lms.size(); i++) {
+      lmsOrder[lms.get(i)] = i;
+    }
+    int[] lmsStr = new int[lms.size()];
+    {
+      int i = 0;
+      for (int suffix : sa) {
+        if (lmsSet.contains(suffix)) {
+          lmsStr[lmsOrder[suffix]] = i;
+          i++;
+        }
+      }
+    }
+    int[] lmsSa = suffixArray(lmsStr);
+
+    // Put the LMS in the array making sure they are sorted
+    sa = new int[n];
+    {
+      Arrays.fill(sa, -1);
+      int[] idx = new int[freq.length];
+      System.arraycopy(freq, 0, idx, 0, freq.length);
+      for (int i = 1; i < idx.length; i++) idx[i] += idx[i - 1];
+      // Ignore the first element since it is the sentinel char ($)
+      for (int i = lmsSa.length - 1; i > 0; i--) {
+        int suffix = lms.get(lmsSa[i]);
+        if (suffix == n - 1) continue; // Ignore the sentinel char ($)
+        int ch = input[suffix];
+        sa[idx[ch]] = suffix;
+        idx[ch]--;
+      }
+      // The sentinel char ($) is the smallest and is always placed at the head
+      sa[0] = n - 1;
+    }
+
+    // Do an induced sorting with the sorted LMS
+    inducedSort(n, sa, input, freq, types);
+
+    return sa;
+  }
+
+  static void inducedSort(int n, int[] sa, int[] input, int[] freq, boolean[] types) {
+    boolean[] prefilled = new boolean[sa.length];
+    for (int i = 0; i < sa.length; i++) if (sa[i] >= 0) prefilled[i] = true;
+
+    // Fill in the L suffixes
+    int[] idx = new int[freq.length];
+    idx[0] = 1;
+    for (int i = 1; i < idx.length; i++) {
+      idx[i] = idx[i - 1] + freq[i - 1];
+    }
+    for (int i = 0; i < n - 1; i++) {
+      if (sa[i] == 0 || sa[i] < 0) continue;
+      // If S(j - 1) is L
+      if (!types[sa[i] - 1]) {
+        int ch = input[sa[i] - 1];
+        sa[idx[ch]] = sa[i] - 1;
+        idx[ch]++;
+      }
+    }
+
+    // Clear the prefilled LMS values
+    for (int i = 0; i < sa.length; i++) if (prefilled[i]) sa[i] = -1;
+
+    // Fill in the S suffixes
+    System.arraycopy(freq, 0, idx, 0, freq.length);
+    for (int i = 1; i < idx.length; i++) idx[i] += idx[i - 1];
+    for (int i = n - 1; i > 0; i--) {
+      if (sa[i] == 0) continue;
+      // If S(i - 1) is type S
+      if (sa[i] >= 0 && types[sa[i] - 1]) {
+        int ch = input[sa[i] - 1];
+        sa[idx[ch]] = sa[i] - 1;
+        idx[ch]--;
+      }
+    }
+    sa[0] = n - 1; // The first element is always the sentinel char ($)
   }
 }
--- src/test/java/net/hydrakecat/SuffixArrayBuilderTest.java
+++ src/test/java/net/hydrakecat/SuffixArrayBuilderTest.java
@@ -1,14 +1,87 @@
 package net.hydrakecat;
 
+import java.util.Arrays;
+import java.util.Random;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SuffixArrayBuilderTest {
+
+  // Suffix Array builder implementation using a naive algorithm that takes O(n^2).
+  // This is used to generate the expected output.
+  private static int[] buildSuffixArrayNaive(CharSequence str) {
+    Pair[] indexSubstrPairs = new Pair[str.length() + 1];
+    for (int i = 0; i < str.length(); i++) {
+      indexSubstrPairs[i] = new Pair(i, str.subSequence(i, str.length()));
+    }
+    indexSubstrPairs[str.length()] = new Pair(str.length(), "");
+    Arrays.sort(indexSubstrPairs);
+
+    int[] ret = new int[indexSubstrPairs.length];
+    for (int i = 0; i < indexSubstrPairs.length; i++) {
+      ret[i] = indexSubstrPairs[i].i;
+    }
+    return ret;
+  }
+
   @Test
   public void build_abracadabra() {
     int[] actual = SuffixArrayBuilder.build("abracadabra");
 
     assertArrayEquals(new int[] {11, 10, 7, 0, 3, 5, 8, 1, 4, 6, 9, 2}, actual);
+  }
+
+  @Test
+  public void build_mmiissiissiippii() {
+    int[] actual = SuffixArrayBuilder.build("mmiissiissiippii");
+
+    assertArrayEquals(new int[] {16, 15, 14, 10, 6, 2, 11, 7, 3, 1, 0, 13, 12, 9, 5, 8, 4}, actual);
+  }
+
+  @Test
+  public void build_randomLowerAlphabet() {
+    Random random = new Random();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 100; i++) {
+      sb.append((char) (random.nextInt('z' - 'a' + 1) + 'a'));
+    }
+    int[] actual = SuffixArrayBuilder.build(sb.toString());
+    int[] expected = buildSuffixArrayNaive(sb.toString());
+
+    assertArrayEquals(expected, actual,
+        "Suffix array for the input string [" + sb + "] do not match");
+  }
+
+  @Test
+  public void build_random() {
+    Random random = new Random();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 100; i++) {
+      sb.append((char) (random.nextInt('z' - 'A' + 1) + 'A'));
+    }
+    try {
+      int[] actual = SuffixArrayBuilder.build(sb.toString());
+      int[] expected = buildSuffixArrayNaive(sb.toString());
+
+      assertArrayEquals(expected, actual,
+          "Suffix array for the input string [" + sb + "] do not match");
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to build suffix array. Input: [" + sb + "]", e);
+    }
+  }
+
+  private static class Pair implements Comparable<Pair> {
+    final int i;
+    final CharSequence str;
+
+    private Pair(int i, CharSequence str) {
+      this.i = i;
+      this.str = str;
+    }
+
+    @Override public int compareTo(Pair o) {
+      return CharSequence.compare(str, o.str);
+    }
   }
 }
