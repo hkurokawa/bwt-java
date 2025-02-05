bc0f23c223b18f32d30e02cc830958f9edac1929
Hiroshi Kurokawa <hirosh.kurokawa@gmail.com>
2025-02-05 23:54:20 +0900

    Compress the alphabets in the input to reduce memory footprint

--- src/main/java/net/hydrakecat/SuffixArrayBuilder.java
+++ src/main/java/net/hydrakecat/SuffixArrayBuilder.java
@@ -2,9 +2,12 @@ package net.hydrakecat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SuffixArrayBuilder {
   /**
@@ -14,32 +17,45 @@ public class SuffixArrayBuilder {
    * @return the suffix array for the provided string
    */
   public static int[] build(CharSequence str) {
-    int[] chars = new int[str.length()];
-    for (int i = 0; i < str.length(); i++) {
-      chars[i] = str.charAt(i);
+    int[] chars = str.chars().boxed().collect(Collectors.toSet()).stream().mapToInt(i -> i).sorted()
+        .toArray();
+    Map<Integer, Integer> mapping = new HashMap<>();
+    for (int i = 0; i < chars.length; i++) {
+      mapping.put(chars[i], i);
     }
-    return suffixArray(chars);
+    int[] input = new int[str.length()];
+    for (int i = 0; i < input.length; i++) {
+      input[i] = mapping.get((int) str.charAt(i));
+    }
+    return suffixArray(input, chars.length);
   }
 
-  static int[] suffixArray(int[] input) {
-    if (input.length == 0) {
+  // Receives the input that consists of a set of integers [0..numUniqueChars] and returns the
+  // suffix array for it.
+  static int[] suffixArray(int[] in, int numUniqueChars) {
+    if (in.length == 0) {
       return new int[] {};
     }
-    if (input.length == 1) {
+    if (in.length == 1) {
       return new int[] {0};
     }
     // Increase the size by 1 for the sentinel char ($)
-    int n = input.length + 1;
+    int n = in.length + 1;
+    numUniqueChars++;
+    int[] input = new int[n];
+    for (int i = 0; i < in.length; i++) {
+      input[i] = in[i] + 1;
+    }
+    input[n - 1] = 0; // the sentinel char ($)
 
-    // Compute the cumulative frequency of the characters excluding the sentinel char ($)
-    int[] freq = new int[Character.MAX_VALUE];
+    // Compute the cumulative frequency of the characters
+    int[] freq = new int[numUniqueChars];
     for (int i : input) freq[i]++;
 
     // Type of the suffixes (true: S, false: L)
     boolean[] types = new boolean[n];
     types[n - 1] = true; // The last suffix ("$") is always type S
-    types[n - 2] = false; // The second to the last suffix is always type L
-    for (int i = n - 3; i >= 0; i--) {
+    for (int i = n - 2; i >= 0; i--) {
       if (input[i] == input[i + 1]) {
         types[i] = types[i + 1];
       } else {
@@ -57,17 +73,13 @@ public class SuffixArrayBuilder {
     int[] sa = new int[n];
     {
       Arrays.fill(sa, -1);
-      int[] idx = new int[freq.length];
-      System.arraycopy(freq, 0, idx, 0, freq.length);
-      for (int i = 1; i < idx.length; i++) idx[i] += idx[i - 1];
+      int[] cend = new int[freq.length];
+      for (int i = 1; i < cend.length; i++) cend[i] = cend[i - 1] + freq[i];
       for (int i : lms) {
-        if (i == n - 1) continue; // Ignore the sentinel char ($)
         int ch = input[i];
-        sa[idx[ch]] = i;
-        idx[ch]--;
+        sa[cend[ch]] = i;
+        cend[ch]--;
       }
-      // The sentinel char ($) is the smallest and is always placed at the head
-      sa[0] = n - 1;
     }
 
     // Do an induced sorting with the bin-sorted LMS
@@ -91,25 +103,20 @@ public class SuffixArrayBuilder {
         }
       }
     }
-    int[] lmsSa = suffixArray(lmsStr);
+    int[] lmsSa = suffixArray(lmsStr, lms.size());
 
     // Put the LMS in the array making sure they are sorted
     sa = new int[n];
     {
       Arrays.fill(sa, -1);
-      int[] idx = new int[freq.length];
-      System.arraycopy(freq, 0, idx, 0, freq.length);
-      for (int i = 1; i < idx.length; i++) idx[i] += idx[i - 1];
-      // Ignore the first element since it is the sentinel char ($)
-      for (int i = lmsSa.length - 1; i > 0; i--) {
+      int[] cend = new int[freq.length];
+      for (int i = 1; i < cend.length; i++) cend[i] = cend[i - 1] + freq[i];
+      for (int i = lmsSa.length - 1; i >= 0; i--) {
         int suffix = lms.get(lmsSa[i]);
-        if (suffix == n - 1) continue; // Ignore the sentinel char ($)
         int ch = input[suffix];
-        sa[idx[ch]] = suffix;
-        idx[ch]--;
+        sa[cend[ch]] = suffix;
+        cend[ch]--;
       }
-      // The sentinel char ($) is the smallest and is always placed at the head
-      sa[0] = n - 1;
     }
 
     // Do an induced sorting with the sorted LMS
@@ -126,18 +133,15 @@ public class SuffixArrayBuilder {
     for (int i = 0; i < sa.length; i++) if (sa[i] >= 0) prefilled[i] = true;
 
     // Fill in the L suffixes
-    int[] idx = new int[freq.length];
-    idx[0] = 1;
-    for (int i = 1; i < idx.length; i++) {
-      idx[i] = idx[i - 1] + freq[i - 1];
-    }
-    for (int i = 0; i < n - 1; i++) {
-      if (sa[i] == 0 || sa[i] < 0) continue;
+    int[] cstart = new int[freq.length];
+    for (int i = 1; i < cstart.length; i++) cstart[i] = cstart[i - 1] + freq[i - 1];
+    for (int i = 0; i < n; i++) {
+      if (sa[i] <= 0) continue;
       // If S(j - 1) is L
       if (!types[sa[i] - 1]) {
         int ch = input[sa[i] - 1];
-        sa[idx[ch]] = sa[i] - 1;
-        idx[ch]++;
+        sa[cstart[ch]] = sa[i] - 1;
+        cstart[ch]++;
       }
     }
 
@@ -145,15 +149,15 @@ public class SuffixArrayBuilder {
     for (int i = 0; i < sa.length; i++) if (prefilled[i]) sa[i] = -1;
 
     // Fill in the S suffixes
-    System.arraycopy(freq, 0, idx, 0, freq.length);
-    for (int i = 1; i < idx.length; i++) idx[i] += idx[i - 1];
-    for (int i = n - 1; i > 0; i--) {
-      if (sa[i] == 0) continue;
+    int[] cend = new int[freq.length];
+    for (int i = 1; i < cend.length; i++) cend[i] = cend[i - 1] + freq[i];
+    for (int i = n - 1; i >= 0; i--) {
+      if (sa[i] <= 0) continue;
       // If S(i - 1) is type S
-      if (sa[i] >= 0 && types[sa[i] - 1]) {
+      if (types[sa[i] - 1]) {
         int ch = input[sa[i] - 1];
-        sa[idx[ch]] = sa[i] - 1;
-        idx[ch]--;
+        sa[cend[ch]] = sa[i] - 1;
+        cend[ch]--;
       }
     }
     sa[0] = n - 1; // The first element is always the sentinel char ($)
